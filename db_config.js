bec21dc751e62518c662c2be2734206f9404e87c
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-06-30 21:40:19 -0500

    feat: :sparkles: Information is added to the db_config.js file
    As the file progresses, the required indexes have already been created and the validations for this project have been generated in each of the fields.

--- db_config.js
+++ db_config.js
@@ -1,4 +1,4 @@
-// Estructura de la coleccion con JSONSchema
+// Estructura de la coleccion usuarios con JSONSchema
 db.createCollection("usuarios", {
     validator: {
         $jsonSchema:{
@@ -25,12 +25,13 @@ db.createCollection("usuarios", {
                     description: "Rol del usuario, debe ser uno de los siguientes: administrador, empleado, cliente"
                 },
                 sede_id: {bsonType: "objectId"}
-            }
+            },
+            additionalProperties: false
         }
     }    
 });
 
-// Estructura de la coleccion con JSONSchema
+// Estructura de la coleccion vehiculos con JSONSchema
 db.createColection("vehiculos", {
     validator: {
         $jsonSchema: {
@@ -107,12 +108,12 @@ db.vehiculos.createIndex(
   { unique: true, partialFilterExpression: { placa: { $exists: true } } }
 );
 
-
+// Estructura de la coleccion sedes con JSONSchema
 db.createCollection("sedes", {
     validator: {
         $jsonSchema: {
             bsonType: "object",
-            required: ["nombre", "direccion", "telefono"],
+            required: ["nombre", "direccion", "ciudad", "telefono"],
             properties: {
                 nombre: {
                     bsonType: "string",
@@ -137,3 +138,90 @@ db.createCollection("sedes", {
         }
     }
 });
+
+// Estructura de la coleccion zonas con JSONSchema
+db.createColection("zonas", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["nombre", "sede_id", "tipos_vehiculo", "capacidad", "cupos_disponibles", "tarifa_hora"],
+            properties: {
+                nombre: {
+                    bsonType: "string",
+                    description: "Nombre de la zona, debe ser un string no vacío"
+                },
+                sede_id: {
+                    bsonType: "objectId",
+                    description: "ID de la sede a la que pertenece la zona, debe ser un ObjectId"
+                },
+                tipos_vehiculo: {
+                    bsonType: "array",
+                    items: { enum: ["carro", "moto", "camion", "bicicleta", "cuatrimoto"] },
+                    description: "Tipos de vehiculo permitidos en la zona, debe ser un array de strings con los siguientes valores: carro, moto, camion, bicicleta, cuatrimoto"
+                },
+                capacidad: {
+                    bsonType: "int",
+                    minimum: 1,
+                    description: "Capacidad total de la zona, debe ser un entero mayor o igual a 1"
+                },
+                cupos_disponibles: {
+                    bsonType: "int",
+                    minimum: 0,
+                    description: "Cupos disponibles en la zona, debe ser un entero mayor o igual a 0"
+                },
+                tarifa_hora: {
+                    bsonType: "double",
+                    minimum: 0,
+                    description: "Tarifa por hora de parqueo en la zona, debe ser un número mayor o igual a 0"
+                }
+            },
+            additionalProperties: false
+        }
+    }
+
+});
+
+// Estructura de la coleccion parqueos con JSONSchema
+db.createColection("parqueos", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["vehiculo_id", "sede_id", "zona_id", "fecha_ingreso", "hora_entrada"],
+            properties: {
+                vehiculo_id: {
+                    bsonType: "objectId",
+                    description: "ID del vehiculo que se esta parqueando, debe ser un ObjectId"
+                },
+                sede_id: {
+                    bsonType: "objectId",
+                    description: "ID de la sede donde se esta parqueando, debe ser un ObjectId"
+                },
+                zona_id: {
+                    bsonType: "objectId",
+                    description: "ID de la zona donde se esta parqueando, debe ser un ObjectId"
+                },
+                hora_entrada: {
+                    bsonType: "date",
+                    description: "Hora de entrada del vehiculo al parqueadero, debe ser un objeto Date"
+                },
+                hora_salida: {
+                    bsonType: "date",
+                    description: "Hora de salida del vehiculo del parqueadero, debe ser un objeto Date"
+                },
+                tiempo_total_min: {
+                    bsonType: "int",
+                    minimum: 0,
+                    description: "Tiempo total de parqueo en minutos, debe ser un entero mayor o igual a 0"
+                },
+                costo: {
+                    bsonType: "double",
+                    minimum: 0,
+                    description: "Costo total del parqueo, debe ser un número mayor o igual a 0"
+                }
+                    
+            },
+            additionalProperties: false
+        }
+    }
+});
+db.parqueos.createIndex({ sede_id:1, zona_id:1});

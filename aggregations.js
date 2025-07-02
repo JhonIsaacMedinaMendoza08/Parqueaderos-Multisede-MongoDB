2e9cf93577b0c2a06b3548a69a7a154ac9b8ef17
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2025-07-02 12:19:37 -0500

    feat: :sparkles: Queries are completed by aggregation
    You must continue defining the roles for each type of user.

--- aggregations.js
+++ aggregations.js
@@ -201,3 +201,141 @@ db.parqueos.aggregate([
         }
     }
 ])
+
+
+// ================================================================
+// 6. Dado un cliente, mostrar su historial de parqueos (fecha, 
+// sede, zona, tipo de vehículo, tiempo y costo).
+// ===============================================================
+
+let clienteId = db.usuarios.findOne({ nombre: "Carlos Mendoza" })._id; 
+
+db.parqueos.aggregate([
+  {
+    $lookup: {
+      from: "vehiculos",
+      localField: "vehiculo_id",
+      foreignField: "_id",
+      as: "vehiculo"
+    }
+  },
+  { $unwind: "$vehiculo" },
+  {
+    $match: { "vehiculo.usuario_id": clienteId }
+  },
+  {    
+    $lookup: {
+      from: "sedes",
+      localField: "sede_id",
+      foreignField: "_id",
+      as: "sede"
+    }},
+    { $unwind: "$sede" },
+    { $lookup: {
+      from: "zonas",
+      localField: "zona_id",
+      foreignField: "_id",
+      as: "zona"
+    }},
+    { $unwind: "$zona" },
+  {
+    $project: {
+        _id: 0,
+        sede_id: "$sede.nombre",
+        zona_id: "$zona.nombre",
+        fecha: "$hora_entrada",
+        tipo_vehiculo: "$vehiculo.tipo",
+        tiempo: "$tiempo_total_min",
+        costo: 1
+    }
+  }
+]);
+
+// ================================================================
+// 7. Mostrar los vehículos parqueados actualmente en cada sede.
+// ================================================================
+db.parqueos.aggregate([
+  {
+    $match: { hora_salida: null }
+  },
+  {
+    $group: {
+      _id: "$sede_id",
+      vehiculosActivos: { $addToSet: "$vehiculo_id" }
+    }
+  },
+    {
+        $lookup: {
+        from: "sedes",
+        localField: "_id",
+        foreignField: "_id",
+        as: "sede"
+        }
+    },
+    { $unwind: "$sede" },
+    {
+        $project: {
+        _id: 0,
+        sede: "$sede.nombre",
+        vehiculosActivos: 1
+        }
+    },
+    {
+        $lookup: {
+        from: "vehiculos",
+        localField: "vehiculosActivos",
+        foreignField: "_id",
+        as: "vehiculos"
+        }
+    },
+    {
+        $project: {
+        sede: 1,
+        vehiculos: {
+            $map: {
+            input: "$vehiculos",
+            as: "vehiculo",
+            in: {
+                tipo: "$$vehiculo.tipo",
+                placa: "$$vehiculo.placa",
+                marca: "$$vehiculo.marca",
+                modelo: "$$vehiculo.modelo"
+            }
+            }
+        }
+        }
+    }   
+]);
+
+// ================================================================     
+// 8. Listar zonas que han excedido su capacidad de parqueo en algún momento.
+// ================================================================
+db.parqueos.aggregate([
+  {
+    $group: {
+      _id: "$zona_id",
+      parqueosTotales: { $sum: 1 }
+    }
+  },
+  {
+    $lookup: {
+      from: "zonas",
+      localField: "_id",
+      foreignField: "_id",
+      as: "zona"
+    }
+  },
+  { $unwind: "$zona" },
+  {
+    $match: {
+      $expr: { $gt: ["$parqueosTotales", "$zona.capacidad"] }
+    }
+  },
+  {
+    $project: {
+      zona: "$zona.nombre",
+      parqueosTotales: 1,
+      capacidad: "$zona.capacidad"
+    }
+  }
+])
